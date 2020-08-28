e9d1cbbc073c3099b0e64fb351a9e9689230c897
traysimay <teh@raptor.pizza>
2020-08-28 07:17:33 -0700

    fonts and better line

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,39 +1,49 @@
-import { easeLinear } from "./easing";
+import { easeLinear } from "./easing"
 
 export const animateAttribute = (ele, att, duration, change) => {
-    return new Promise((resolve, reject) => {
-        const initialValue = parseFloat(ele.getAttribute(att))
-        const startTime = Date.now()
-        const target = initialValue + change
-        let frame;
-        const animate = () => {
-            const elapsedTime = Date.now() - startTime
-            const newValue = easeLinear(elapsedTime, initialValue, change, duration)
-            ele.setAttribute(att, newValue)
-            if ((newValue >= target && change > 0) || (newValue <= target && change < 0)) {
-                ele.setAttribute(att, target)
-                resolve()
-                return cancelAnimationFrame(frame)
-            }
-            frame = requestAnimationFrame(animate)
-
-        }
-        animate()
-    })
+  return new Promise((resolve, reject) => {
+    const initialValue = parseFloat(ele.getAttribute(att))
+    const startTime = Date.now()
+    const target = initialValue + change
+    let frame
+    const animate = () => {
+      const elapsedTime = Date.now() - startTime
+      const newValue = easeLinear(elapsedTime, initialValue, change, duration)
+      ele.setAttribute(att, newValue)
+      if (
+        (newValue >= target && change > 0) ||
+        (newValue <= target && change < 0)
+      ) {
+        ele.setAttribute(att, target)
+        resolve()
+        return cancelAnimationFrame(frame)
+      }
+      frame = requestAnimationFrame(animate)
+    }
+    animate()
+  })
 }
 
-export const animateLine = (lineEl) => {
-    const startX = parseFloat(lineEl.getAttribute('x1'))
-    const endX = parseFloat(lineEl.getAttribute('x2'))
-    lineEl.setAttribute("x2", startX)
-    function start() {
-        animateAttribute(lineEl, "x2", 2000, endX - startX).then(() => {
-            animateAttribute(lineEl, "x1", 2000, (endX - startX)).then(() => {
-                animateAttribute(lineEl, "x1", 2000, (endX - startX) * -1).then(() => {
-                    animateAttribute(lineEl, "x2", 2000, (endX - startX) * -1).then(start)
-                })
-            })
-        })
-    }
-    start()
+export const animateLine = lineEl => {
+  const startX = parseFloat(lineEl.getAttribute("x1"))
+  const endX = parseFloat(lineEl.getAttribute("x2"))
+  lineEl.setAttribute("x2", startX)
+  let stop = false
+  async function start() {
+    const randomInterval = () => 2000 * Math.random()
+    await animateAttribute(lineEl, "x2", randomInterval(), endX - startX)
+    await animateAttribute(lineEl, "x1", randomInterval(), endX - startX)
+    await animateAttribute(lineEl, "y2", randomInterval(), -200)
+    await animateAttribute(lineEl, "y1", randomInterval(), -200)
+    await animateAttribute(lineEl, "x1", randomInterval(), (endX - startX) * -1)
+    await animateAttribute(lineEl, "x2", randomInterval(), (endX - startX) * -1)
+    await animateAttribute(lineEl, "y2", randomInterval(), 200)
+    await animateAttribute(lineEl, "y1", randomInterval(), 200)
+    if (!stop) start()
+  }
+  start()
+  return () => {
+    stop = true
+    console.log("stawp")
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,25 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import SVG from "react-inlinesvg"
-import { c1, c2, c3, c4, c5, c6, c7, enterButton, enterLine } from "../selectors/enter";
-import { easeLinear, animateAttribute, animateLine } from "../animations";
-import { radius } from "../selectors/attributes";
+import {
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  enterButton,
+  enterLine,
+} from "../selectors/enter"
+import { easeLinear, animateAttribute, animateLine } from "../animations"
+import { radius } from "../selectors/attributes"
 import styled, { css, keyframes } from "styled-components"
 import * as Tone from "tone"
 import "../animations/animations.css"
 
 function createCSS() {
-  let styles = ``;
+  let styles = ``
 
   for (let i = 1; i < 8; i += 1) {
     styles += `
@@ -25,46 +35,56 @@ function createCSS() {
      `
   }
 
-  return css`${styles}`;
+  return css`
+    ${styles}
+  `
 }
 const AnimWrap = styled.div`
-svg {
-  overflow:visible
-}
+  svg {
+    overflow: visible;
+  }
   ${createCSS()};
 `
 const IndexPage = () => {
-
   const setup = () => {
-    enterButton().style.animation = 'red-pulse 5000ms infinite alternate'
-    enterButton().querySelector('text').style.strokeWidth = "3px"
-    enterButton().querySelector('text').style.animation = 'stroke-pulse 5000ms infinite alternate'
-    animateLine(enterLine())
-    document.querySelectorAll('circle.cls-1').forEach((el, i) => {
-      const cx = el.getAttribute('cx')
-      const cy = el.getAttribute('cy')
+    enterButton().style.animation = "red-pulse 5000ms infinite alternate"
+    enterButton().querySelector("text").style.strokeWidth = "3px"
+    enterButton().querySelector("text").style.animation =
+      "stroke-pulse 5000ms infinite alternate"
+
+    enterLine().style.stroke = "white"
+    const stop = animateLine(enterLine())
+    enterButton().onclick = stop
+
+    document.querySelectorAll("circle.cls-1").forEach((el, i) => {
+      const cx = el.getAttribute("cx")
+      const cy = el.getAttribute("cy")
       el.style.transformOrigin = `${cx}px ${cy}px`
       el.onclick = () => {
-        const c = i % 4 + 2
-        Tone.start()
-        const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+        const c = (i % 4) + 2
+        const synth = new Tone.PolySynth(Tone.Synth).toDestination()
         const now = Tone.now()
-        synth.triggerAttack(`D${c}`, now);
-        synth.triggerAttack(`F${c}`, now + 0.5);
-        synth.triggerAttack(`A${c}`, now + 1);
-        synth.triggerAttack(`C${c + 1}`, now + 1.5);
-        synth.triggerAttack(`E${c + 1}`, now + 2);
-        synth.triggerRelease([`D${c}`, `F${c}`, `A${c}`, `C${c + 1}`, `E${c + 1}`], now + 4);
-      };
+        synth.triggerAttack(`D${c}`, now)
+        synth.triggerAttack(`F${c}`, now + 0.5)
+        synth.triggerAttack(`A${c}`, now + 1)
+        synth.triggerAttack(`C${c + 1}`, now + 1.5)
+        synth.triggerAttack(`E${c + 1}`, now + 2)
+        synth.triggerRelease(
+          [`D${c}`, `F${c}`, `A${c}`, `C${c + 1}`, `E${c + 1}`],
+          now + 4
+        )
+      }
     })
   }
 
-  return (<Layout>
-    <SEO title="Home" />
-    <AnimWrap>
-      <SVG src={require("../SVG/ENTER.svg")} onLoad={setup} />
-    </AnimWrap>
-  </Layout>)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <AnimWrap>
+        <SVG src={require("../SVG/ENTER.svg")} onLoad={setup} />
+      </AnimWrap>
+    </Layout>
+  )
 }
 
 export default IndexPage
