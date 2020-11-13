f2eb9c381370e99f5701cec47a57ed2ee376fc4e
traysimay <teh@raptor.pizza>
2020-11-13 14:20:48 -0800

    new stuff

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -19,7 +19,53 @@ const getViewScalars = () => {
   return { scalarX, scalarY }
 }
 
+const getCircleAtts = el => {
+  const cx = el.getAttribute("cx")
+  const cy = el.getAttribute("cy")
+  const r = el.getAttribute("r")
+  return { cx, cy, r }
+}
+
+const getTextAtts = el => {
+  const translate = el
+    .getAttribute("transform")
+    .split("translate(")[1]
+    .split(")")[0]
+    .split(" ")
+  const tX = parseFloat(translate[0])
+  const tY = parseFloat(translate[1])
+  const fontSize = parseFloat(el.style.fontSize.replace("px", ""))
+  return { tX, tY, fontSize }
+}
+
 const SecondPage = () => {
+  const setupMenu = () => {
+    const menuButtonG = document.querySelector("#menu-2")
+    const menuButtonCircle = menuButtonG.querySelector("circle")
+    const menuButtonText = menuButtonG.querySelector("text")
+    const menuButtonTargetG = document.querySelector("#menu")
+
+    const menuButtonCircleAtts = getCircleAtts(menuButtonCircle)
+    const menuButtonTextAtts = getTextAtts(menuButtonG.querySelector("text"))
+
+    const menuButtonTargetCircleAtts = getCircleAtts(
+      menuButtonTargetG.querySelector("circle")
+    )
+    const menuButtonTargetTextAtts = getTextAtts(
+      menuButtonTargetG.querySelector("text")
+    )
+
+    menuButtonCircle.setAttribute("cx", menuButtonTargetCircleAtts.cx)
+    menuButtonCircle.setAttribute("cy", menuButtonTargetCircleAtts.cy)
+    menuButtonCircle.setAttribute("r", menuButtonTargetCircleAtts.r)
+
+    menuButtonText.setAttribute(
+      "transform",
+      `translate(${menuButtonTargetTextAtts.tX} ${menuButtonTargetTextAtts.tY})`
+    )
+    menuButtonText.style.fontSize = `${menuButtonTargetTextAtts.fontSize}px`
+  }
+
   const setup = async () => {
     const { scalarX, scalarY } = getViewScalars()
     const { x, y, width, height } = raptor().getBoundingClientRect()
@@ -37,6 +83,8 @@ const SecondPage = () => {
     raptor().style.transform = "scale(1)"
     await delay(3000)
     raptor().style.animation = "spinning 30ms 10 linear"
+
+    setupMenu()
   }
   return (
     <Layout>
